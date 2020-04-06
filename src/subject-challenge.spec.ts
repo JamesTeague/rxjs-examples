ba3eb262b33522bb22e5a7a19e3b667920899cf5
James Teague <jteague@bluebeam.com>
2020-04-06 08:15:32 -0500

    add challenge to convert to use subjects

---
+++ src/subject-challenge.spec.ts
@@ -0,0 +1,51 @@
+import { Observable, Subject } from 'rxjs';
+import Notifier from './subject-challenge';
+
+describe('Notifier', () => {
+  let memoryNotifier;
+
+  beforeEach(() => {
+    memoryNotifier = new Notifier();
+  });
+
+  it('returns a new observable', () => {
+    const observable$ = memoryNotifier.topic('test');
+
+    expect(observable$).toBeInstanceOf(Observable);
+  });
+
+  it('emits a message to the subscriber', done => {
+    const observable$ = memoryNotifier.topic('test');
+
+    observable$.subscribe(data => {
+      expect(data).toEqual({ payload: 'test' });
+      done();
+    });
+
+    memoryNotifier.broadcast('test', { payload: 'test' });
+  });
+
+  it('is multicasted', done => {
+    let calls = 0;
+    const callLimit = 2;
+    const emittedData: any[] = [];
+
+    const recordData = (data: any) => {
+      calls++;
+      emittedData.push(data);
+
+      if (calls === callLimit) {
+        expect(emittedData).toHaveLength(2);
+        expect(emittedData[0]).toMatchObject(emittedData[1]);
+        done();
+      }
+    };
+
+    const observable$ = memoryNotifier.topic('test');
+
+    observable$.subscribe(recordData);
+    observable$.subscribe(recordData);
+
+    memoryNotifier.broadcast('test', { payload: 'test' });
+  });
+});
---
+++ src/subject-challenge.ts
@@ -0,0 +1,43 @@
+import { Observable, Observer, Subscriber } from 'rxjs';
+
+export default class Notifier {
+  private readonly subscribers: Map<string, Array<Subscriber<any>>>;
+
+  constructor() {
+    this.subscribers = new Map();
+  }
+
+  public topic(key: string) {
+    return new Observable(observer => {
+      if (!this.subscribers.get(key)) {
+        this.subscribers.set(key, []);
+      }
+
+      (this.subscribers.get(key) as Array<Subscriber<any>>).push(observer);
+
+      return () => {
+        const subs = this.subscribers.get(key) as Array<Subscriber<any>>;
+        const index = subs.indexOf(observer);
+
+        if (index !==-1) {
+          subs.splice(index, 1);
+        }
+
+        if (subs.length === 0) {
+          this.subscribers.delete(key);
+        }
+      }
+    })
+  }
+
+  public broadcast(topic, message) {
+    const subs = this.subscribers.get(topic);
+    if (subs) {
+      subs.forEach(subscription => subscription.next(message || ''));
+    }
+  }
+}
+
+const notifier = new Notifier();
+
+notifier.topic('Challenge');
